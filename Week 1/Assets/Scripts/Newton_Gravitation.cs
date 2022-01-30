b5f373c369f556c7e81bd5f793a7404c6e4a8f51
IreliaLeaf <irelialeaf2021@protonmail.com>
2022-01-30 19:31:49 +0000

    I created the files

---
+++ Week 1/Assets/Scripts/Newton_Gravitation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Newton_Gravitation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject [] Objects =
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        
+    }
+}
