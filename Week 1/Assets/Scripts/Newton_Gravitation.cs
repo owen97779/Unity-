610036b66be456fc6598cddc6308fdec29ac5806
Mohammad Shikha <speedway45@live.co.uk>
2022-01-31 16:33:18 +0000

    Working model from youtube tutorial

--- Week 1/Assets/Scripts/Newton_Gravitation.cs
+++ Week 1/Assets/Scripts/Newton_Gravitation.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Newton_Gravitation : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        GameObject [] Objects =
-    }
-
+{  
+    //Calling Rigidbody 
+    public Rigidbody rb;
     // Update is called once per frame
-    void FixedUpdate() {
-        
+    void FixedUpdate() 
+    {
+        Newton_Gravitation[] objects = FindObjectsOfType<Newton_Gravitation>();
+        foreach (Newton_Gravitation i in objects)
+        {
+            if (i != this)
+                Attraction(i);
+        }
+
+    }
+    void Attraction (Newton_Gravitation objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+        float forceMagnitude = (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+        rbToAttract.AddForce(force);
     }
 }
