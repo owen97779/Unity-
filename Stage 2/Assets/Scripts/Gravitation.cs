f638922f5c4bcaac82bfce97264160854f71ab0b
IreliaLeaf <irelialeaf2021@protonmail.com>
2022-02-08 20:49:32 +0000

    Stage 2

---
+++ Stage 2/Assets/Scripts/Gravitation.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravitation : MonoBehaviour
+{
+    //public GameObject Planet;
+    public int G;
+    Planets[] objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objects = FindObjectsOfType<Planets>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for (var i = 0; i < objects.Length-1; i++)
+        {
+            for (var j = i+1; j < objects.Length; j++)
+            {
+                Force(i,j);
+            }
+        }
+        for (var i = 0; i < objects.Length; i++)
+        {Movement(i);}
+    }
+
+    void Force(int i, int j)
+    {
+        Planets ObjectSubject= objects[i];
+        Planets ObjectObject = objects[j];
+        Rigidbody rb1 = ObjectSubject.GetRigidbody();
+        Rigidbody rb2 = ObjectObject.GetRigidbody();
+        Vector3 position1 = rb1.position;
+        Vector3 position2 = rb2.position;
+        Vector3 force = G*rb1.mass*rb2.mass*((position2-position1).normalized)/(Mathf.Pow((position2-position1).magnitude,2));
+        ObjectSubject.SetForce(force);
+        ObjectObject.SetForce(force * (-1));
+    }
+    void Movement(int i)
+    {
+        objects[i].Movement();
+    }
+        
+    
+}
---
+++ Stage 2/Assets/Scripts/Planets.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody Sphere;
+    public Vector3 velocity;
+    Vector3 Force;
+    void Start()
+    {
+        Force = Vector3.zero;
+        Sphere.velocity = velocity;
+    }
+
+    public Rigidbody GetRigidbody()
+    {
+        return Sphere;
+    }
+
+    public void SetForce(Vector3 force)
+    {
+        Force = force + Force;
+    }
+
+    public void Movement()
+    {
+        Sphere.AddForce(Force);
+    }
+
+    private void FixedUpdate() 
+    {
+        Force = Vector3.zero; 
+    }
+
+}
